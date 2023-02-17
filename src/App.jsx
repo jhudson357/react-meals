1a3e96e381b6fc5001a61b340213846affed0a1c
James Hudson <jmh357@comcast.net>
2023-02-17 15:14:53 -0600

    implement useContext to update cart item amt;

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
 
   const showCartHandler = () => {
-    console.log('cart clicked')
     setCartIsShown(true)
   }
 
@@ -16,13 +16,13 @@ function App() {
   }
   
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   )
 }
 
