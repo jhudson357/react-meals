88b9d46e7b03a42edd0a81d76adeb94abd18514e
James Hudson <jmh357@comcast.net>
2023-03-07 12:06:05 -0600

    implement clearing the cart functionality, add success msg

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,8 @@ import Checkout from './Checkout'
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false)
-  
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [didSubmit, setDidSubmit] = useState(false)
   const cartCtx = useContext(CartContext)
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -24,15 +25,20 @@ const Cart = (props) => {
   const orderHandler = () => {
     setIsCheckout(true)
   }
+  console.log(cartCtx)
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://react-http-790d2-default-rtdb.firebaseio.com/orders.json', {
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true)
+    await fetch('https://react-http-790d2-default-rtdb.firebaseio.com/orders.json', {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
         orderedItems: cartCtx.items
       }),
     })
+    setIsSubmitting(false)
+    setDidSubmit(true)
+    cartCtx.clearCart()
   }
 
   const cartItems = (
@@ -60,8 +66,8 @@ const Cart = (props) => {
       {hasItems && <button className={styles.button} onClick={orderHandler} >Order</button>}
     </div>
   
-  return (
-    <Modal onBackdropClick={props.onHideCart}>
+  const cartModalContent = 
+    <>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
@@ -69,6 +75,27 @@ const Cart = (props) => {
       </div>
       {isCheckout && <Checkout onCancel={props.onHideCart} onConfirm={submitOrderHandler}/>}
       {!isCheckout && modalActions}
+    </>
+
+  const isSubmittingModalContent = <p>Sending order data...</p>
+
+  const didSubmitModalContent = 
+    <>
+      <p>Successfully sent the order!</p>
+      <div className={styles.actions}>
+      <button 
+        className={styles['button']} 
+        onClick={props.onHideCart}>
+          Close
+      </button>
+    </div>
+    </>
+
+  return (
+    <Modal onBackdropClick={props.onHideCart}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   )
 }
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -54,6 +54,11 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     }
   }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState
+  }
+
   return defaultCartState
 }
 
@@ -74,11 +79,18 @@ const CartProvider = (props) => {
     })
   }
 
+  const clearCartHandler = () => {
+    dispatchCartAction({
+      type: 'CLEAR'
+    })
+  }
+
   const cartContext = {
   items: cartState.items,
   totalAmount: cartState.totalAmount,
   addItem: addItemToCartHandler,
   removeItem: removeItemToCartHandler,
+  clearCart: clearCartHandler,
 }
   
   return (
