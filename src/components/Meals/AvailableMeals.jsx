17b38c7224550fd94cd6e5dc928f93543ee2f8e3
James Hudson <jmh357@comcast.net>
2023-03-07 10:14:46 -0600

    add error handling to http request

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -8,10 +8,16 @@ import MealItem from './MealItem/MealItem'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState ()
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-790d2-default-rtdb.firebaseio.com//meals.json')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -29,13 +35,24 @@ const AvailableMeals = () => {
       setIsLoading(false)
     }
 
-    fetchMeals()
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
 
   if (isLoading) {
     return (
       <section className={styles.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
